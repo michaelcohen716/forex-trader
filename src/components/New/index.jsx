320004f6df912c071984e4c4c72596304cea225f
michaelcohen716 <michaelcohen716@gmail.com>
2019-12-09 23:04:23 -0500

    continue frontend

--- src/components/New/index.jsx
+++ src/components/New/index.jsx
@@ -4,7 +4,7 @@ import Select from "react-select";
 import "./new.css";
 
 const TRADE_OPTIONS = ["Long", "Short"];
-const CURRENCY_OPTIONS = ["USD", "EUR", "JPY", "GBP"]
+const CURRENCY_OPTIONS = ["USD", "EUR", "JPY", "GBP"];
 const VALUE_OPTIONS = ["0.1", "1", "10", "100"];
 
 const tradeOptions = TRADE_OPTIONS.map(o => {
@@ -31,23 +31,38 @@ const valueOptions = VALUE_OPTIONS.map(o => {
 function New() {
   const [tradeOption, setTradeOption] = useState();
   const [currencyOption, setCurrencyOption] = useState();
+  const [currencyOption2, setCurrencyOption2] = useState();
   const [valueOption, setValueOption] = useState();
+
+  const [formFields, toggleFormFields] = useState([false, false, false, false]);
+
+  const setFormFields = idx => {
+    const fields = [...formFields];
+    fields[idx] = true;
+    toggleFormFields(fields);
+  };
 
   const handleTradeChange = selectedOption => {
     setTradeOption(selectedOption);
+    setFormFields(0);
   };
 
-  const handleCurrencyChange = selectedOption => {
+  const handleMyAssetChange = selectedOption => {
     setCurrencyOption(selectedOption);
+    setFormFields(1);
+  };
+
+  const handleCounterpartyAssetChange = selectedOption => {
+    setCurrencyOption2(selectedOption);
+    setFormFields(2);
   };
 
   const handleValueChange = selectedOption => {
     setValueOption(selectedOption);
+    setFormFields(3);
   };
 
-  console.log('trade option', tradeOption)
-  console.log('currency option', currencyOption)
-  console.log('value option', valueOption)
+  console.log("formfields", formFields);
 
   return (
     <div className="d-flex flex-column">
@@ -58,32 +73,40 @@ function New() {
           <Select
             className="react-select-container ml-2"
             value={tradeOption}
-            onChange={handleTradeChange}
+            onChange={handleTradeChange} // trade
             options={tradeOptions}
             placeholder="Position"
-            />
+          />
           <Select
             className="react-select-container ml-2"
             value={currencyOption}
-            onChange={handleCurrencyChange}
+            onChange={handleMyAssetChange} //myasset
             options={currencyOptions}
-            placeholder="Currency"
-            />
-        </div>
-      </div>
-      <div className="option-line d-flex mx-auto mt-4">
-        <div className="d-flex">
-          <div className="decision-point my-auto">trading against</div>
-          <Select
-            className="react-select-container ml-2"
-            value={tradeOption}
-            onChange={handleTradeChange}
-            options={tradeOptions}
             placeholder="Currency"
           />
         </div>
       </div>
-      <div className="option-line d-flex mx-auto mt-4">
+      <div
+        className={`option-line d-flex mx-auto mt-4 ${(!formFields[1] ||
+          !formFields[0]) &&
+          "add-opacity"}`}
+      >
+        <div className="d-flex">
+          <div className="decision-point my-auto">trading against</div>
+          <Select
+            className="react-select-container ml-2"
+            value={currencyOption2}
+            onChange={handleCounterpartyAssetChange} //counterpartyasset
+            options={currencyOptions.filter(curr => curr !== currencyOption)}
+            isDisabled={!formFields[1] || !formFields[0]}
+            placeholder="Currency"
+          />
+        </div>
+      </div>
+      <div
+        className={`option-line d-flex mx-auto mt-4 ${!formFields[2] &&
+          "add-opacity"}`}
+      >
         <div className="d-flex">
           <div className="decision-point my-auto">at a notional value of</div>
           <Select
@@ -93,9 +116,25 @@ function New() {
             options={valueOptions}
             placeholder="Value"
           />
-          <div className="decision-point my-auto ml-2">ETH</div>
+          <div className="decision-point my-auto ml-2">ETH.</div>
         </div>
       </div>
+      <div
+        className={`option-line d-flex mx-auto mt-4 ${!formFields[3] &&
+          "add-opacity"}`}
+      >
+        <div className="d-flex">
+          <div className="decision-point my-auto">
+            I will submit {valueOption ? Number(valueOption.value / 10) : 0} ETH as collateral.
+          </div>
+        </div>
+      </div>
+      <button
+        className="submit-button mx-auto mt-4 p-2"
+        disabled={formFields.filter(ff => ff === false).length}
+      >
+        Submit
+      </button>
     </div>
   );
 }
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -4,9 +4,9 @@ import "./Nav.css";
 function TopNav() {
   return (
     <div className="top-nav mx-auto mt-3 mb-2 d-flex justify-content-end">
-      <div class="dropdown account">
-        <button class="dropbtn"></button>
-        <div class="dropdown-content">
+      <div className="dropdown account">
+        <button className="dropbtn"></button>
+        <div className="dropdown-content">
           <div className="d-flex flex-column">
             <div>
                 0x48nbf389h44
